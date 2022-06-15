e234120a5fbb1289df1c16e1cb1a12b0fe3241c0
HP <vickyvalerian27@gmail.com>
2022-06-15 22:49:02 +0700

    hosting

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ import axios from 'axios';
 Vue.config.productionTip = false;
 
 Vue.prototype.$http = axios;
-Vue.prototype.$baseUrl = 'http://127.0.0.1:8000';
-Vue.prototype.$api = 'http://127.0.0.1:8000/api';
+Vue.prototype.$baseUrl = 'https://190710079.atmajogjarental.xyz/api/public';
+Vue.prototype.$api = 'https://190710079.atmajogjarental.xyz/api/api';
 
 new Vue({
   vuetify,
